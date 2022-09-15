2d967cfd795540efde26b9053eeba52212438f8c
4rch <misael.leal.goncalves90@gmail.com>
2022-09-14 21:57:39 -0400

    #1

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+ 
+
+
+import productsRoutes from "./routes/products.routes"
+const app = express();
+
+
+
+//settings
+app.set('view engine', 'ejs')
+
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) =>{
+    res.render(__dirname+ '/views/initial.ejs')
+})
+
+app.use('/products',productsRoutes)
+
+export default app
---
+++ src/controllers/products.controller.js
@@ -0,0 +1,15 @@
+export const createProduct = (req, res) =>{
+
+}
+export const getProducts = (req, res) =>{
+    res.json('Get Products')
+}
+export const getProductById= (req, res) =>{
+    
+}
+export const updateProductById = (req, res) =>{
+    
+}
+export const deleteProductById = (req, res) =>{
+    
+}
